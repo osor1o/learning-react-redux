e2e376b165f1cf343e17ce58258afc2907ae7545
Osorio Neto <osor1o@hotmail.com>
2019-07-26 02:17:21 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Counter from './counter'
+import counterReducer from './counterReducer';
+
+const reducers = combineReducers({
+    counter: counterReducer
+})
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <Counter></Counter>
+    </Provider>
+, document.getElementById('app'))
