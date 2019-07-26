eb22a82a3d824b7ce65efdccd1b93f32eea0aef7
Osorio Neto <osor1o@hotmail.com>
2019-07-26 02:26:15 -0300

    added new folder structure

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import Counter from './counter'
-import counterReducer from './counterReducer';
+import Counter from './components/containers/counter/counter'
+import counterReducer from './components/containers/counter/counterReducer';
 
 const reducers = combineReducers({
     counter: counterReducer
